c00e3f58d7776a5ad92ff7be3dda41e245b634f9
Amit Nagdev <amit@Amits-MacBook-Pro.local>
2018-11-18 18:42:22 -0800

    APIHandler Altered to accomodate bookmarking

--- app/src/main/java/scu/csci187/fall2018/mealtracker/Classes/APIHandler.java
+++ app/src/main/java/scu/csci187/fall2018/mealtracker/Classes/APIHandler.java
@@ -1,15 +1,15 @@
 package scu.csci187.fall2018.mealtracker.Classes;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.json.JSONTokener;
-import org.json.JSONArray;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.StringReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import android.os.AsyncTask;
+        import org.json.JSONException;
+        import org.json.JSONObject;
+        import org.json.JSONTokener;
+        import org.json.JSONArray;
+        import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+        import java.io.StringReader;
+        import java.net.MalformedURLException;
+        import java.net.URL;
+        import android.os.AsyncTask;
 
 
 
@@ -25,11 +25,32 @@ public class APIHandler extends AsyncTask<QueryParam, Void , Query> {
         // TODO For reference. Please remove later. String test = "https://api.edamam.com/search?q=chicken&app_id=b957081d&app_key=889e79d32df59ed1621b6247b075e26a&from=0&to=3&calories=591-722&health=alcohol-free";
 
         // Declare necessary variables for getting JSON from API based on search
-        StringBuilder stringBuilder = new StringBuilder();
+
         JSONObject json = null;
 
 
         String assembledQuery = (myParams[0]).assembleSearchURL();
+
+        json = getJSONFromURL(assembledQuery);
+
+        // Get values. Currently a test to get recipe data:
+
+        String resultsKeyword = "hits";
+        JSONArray searchResults;
+        try {
+            searchResults = json.getJSONArray(resultsKeyword);
+        } catch (JSONException e) {
+            return null;
+        }
+
+        Query query = new Query(searchResults);
+        return query;
+
+    }
+
+    public JSONObject getJSONFromURL (String assembledQuery) {
+
+        StringBuilder stringBuilder = new StringBuilder();
 
         // Get JSON from API
         try {
@@ -46,30 +67,18 @@ public class APIHandler extends AsyncTask<QueryParam, Void , Query> {
                 }
 
                 try {
-                    json = (JSONObject) new JSONTokener(stringBuilder.toString()).nextValue();
+                    return (JSONObject) new JSONTokener(stringBuilder.toString()).nextValue();
                 } catch (JSONException e) {
                     return null;
                 }
             }
         } catch (MalformedURLException e) {
             System.out.println("Error = " + e.getMessage());
+            return null;
         } catch (IOException e) {
             System.out.println("Error with IO in URL accession");
-        }
-
-        // Get values. Currently a test to get recipe data:
-
-        String resultsKeyword = "hits";
-        JSONArray searchResults;
-        try {
-            searchResults = json.getJSONArray(resultsKeyword);
-        } catch (JSONException e) {
             return null;
         }
-
-        Query query = new Query(searchResults);
-        return query;
-
     }
 
 }
