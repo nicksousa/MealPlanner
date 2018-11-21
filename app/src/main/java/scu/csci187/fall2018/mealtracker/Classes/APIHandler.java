b106fd3417db731f7ed49cb8e803173235367369
amitnagdev <amit@nagdev.com>
2018-11-20 23:54:29 -0800

    Working search fragment (searching works. Rest is still being worked on)
    Built the backend for the Search Fragment. It now can search the API.
    
    APIHandler was modified to allow for two separate courses of action
    depending on whether it was asked to carry out a search or bookmarked
    recipe retrieval.
    
    In MainActivity the sendPreferences function was removed because it was
    made redundant.

--- app/src/main/java/scu/csci187/fall2018/mealtracker/Classes/APIHandler.java
+++ app/src/main/java/scu/csci187/fall2018/mealtracker/Classes/APIHandler.java
@@ -16,20 +16,22 @@ package scu.csci187.fall2018.mealtracker.Classes;
 
 public class APIHandler extends AsyncTask<String, Void, JSONObject> {
 
-    public Query search(QueryParam qp) {
+    boolean isSearch = false;
 
+    public Query search(QueryParam qp) {
+        isSearch = true;
 
         // TODO For reference. Please remove later. String test = "https://api.edamam.com/search?q=chicken&app_id=b957081d&app_key=889e79d32df59ed1621b6247b075e26a&from=0&to=3&calories=591-722&health=alcohol-free";
 
         // Declare necessary variables for getting JSON from API based on search
 
-        JSONObject json = null;
+        JSONObject jsonObject = null;
 
 
         String assembledQuery = qp.assembleSearchURL();
 
         try {
-            json = this.execute(assembledQuery).get();
+            jsonObject = this.execute(assembledQuery).get();
         } catch (ExecutionException e) {
             e.printStackTrace();
         } catch (InterruptedException e) {
@@ -40,13 +42,22 @@ public class APIHandler extends AsyncTask<String, Void, JSONObject> {
 
         String resultsKeyword = "hits";
         JSONArray searchResults;
+        if (jsonObject == null) {
+            try {
+                jsonObject = new JSONObject("{weewoo: -1}");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
         try {
-            searchResults = json.getJSONArray(resultsKeyword);
+            searchResults = jsonObject.getJSONArray(resultsKeyword);
         } catch (JSONException e) {
             return null;
         }
 
         Query query = new Query(searchResults);
+        isSearch = false;
         return query;
 
     }
@@ -71,13 +82,24 @@ public class APIHandler extends AsyncTask<String, Void, JSONObject> {
                     stringBuilder.append(line);
                 }
 
-                try {
-                    JSONArray jsonArray = ( (JSONArray) new JSONTokener(stringBuilder.toString()).nextValue() );
-                    return jsonArray.getJSONObject(0);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                    return null;
+                if (this.isSearch) {
+                    try {
+                        JSONObject jsonObject = ( (JSONObject) new JSONTokener(stringBuilder.toString()).nextValue() );
+                        return jsonObject;
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        return null;
+                    }
+                } else {
+                    try {
+                        JSONArray jsonArray = ( (JSONArray) new JSONTokener(stringBuilder.toString()).nextValue() );
+                        return jsonArray.getJSONObject(0);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        return null;
+                    }
                 }
+
             }
         } catch (MalformedURLException e) {
             System.out.println("Error = " + e.getMessage());
