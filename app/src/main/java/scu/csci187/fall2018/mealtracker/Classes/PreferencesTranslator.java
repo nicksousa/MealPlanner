8dd989f1f17edb937a584d1d00c3ce5822ba9239
amitnagdev <amit@nagdev.com>
2018-11-21 19:47:32 -0800

    FIXED bug with Searching preventing no parameter searches
    Bug was that you couldn't just search for something and needed
    to enter details into the advanced tab, defeating the purpose of
    having an advanced tab.
    
    Fixed by handling default values and checking null userPrefs

--- app/src/main/java/scu/csci187/fall2018/mealtracker/Classes/PreferencesTranslator.java
+++ app/src/main/java/scu/csci187/fall2018/mealtracker/Classes/PreferencesTranslator.java
@@ -11,68 +11,86 @@ public class PreferencesTranslator {
     }
 
     public void setDietInQueryParam(UserPreferences userPref, QueryParam qp) {
-
-        switch (userPref.getDietLabel()) {
-            case -1:    qp.setDiet("");
-                        return;
-            case 0:     qp.setDiet("low-carb");
-                        return;
-            case 1:     qp.setDiet("low-fat");
-                        return;
-            case 2:     qp.setDiet("high-protein");
-                        return;
-            case 3:     qp.setDiet("high-fiber");
-                        return;
-            case 4:     qp.setDiet("low-sodium");
-                        return;
-            default:    qp.setDiet("");
-                        return;
+        if (userPref == null) {
+            qp.setDiet("");
+        } else {
+            switch (userPref.getDietLabel()) {
+                case -1:
+                    qp.setDiet("");
+                    return;
+                case 0:
+                    qp.setDiet("low-carb");
+                    return;
+                case 1:
+                    qp.setDiet("low-fat");
+                    return;
+                case 2:
+                    qp.setDiet("high-protein");
+                    return;
+                case 3:
+                    qp.setDiet("high-fiber");
+                    return;
+                case 4:
+                    qp.setDiet("low-sodium");
+                    return;
+                default:
+                    qp.setDiet("");
+                    return;
+            }
         }
 
     }
 
     public void setHealthLabelsInQueryParam(UserPreferences userPref, QueryParam qp) {
 
-        qp.setCalorieMin(userPref.getCalorieLow());
-        qp.setCalorieMax(userPref.getCalorieHigh());
-        qp.setTimeMin(0);
-        qp.setTimeMax(userPref.getMaxTimeInMinutes());
+        if (userPref == null) {
+            // userPref not set so everything gets default value
+            // to be handled by QueryParam upon assembly.
+            qp.setCalorieMin(0);
+            qp.setCalorieMax(0);
+            qp.setTimeMin(0);
+            qp.setTimeMax(0);
+        } else {
+            qp.setCalorieMin(userPref.getCalorieLow());
+            qp.setCalorieMax(userPref.getCalorieHigh());
+            qp.setTimeMin(0);
+            qp.setTimeMax(userPref.getMaxTimeInMinutes());
 
 
-        // TODO check these are correct terms.
-        if (userPref.isVegan()){
-            qp.addHealthLabels("vegan");
+            // TODO check these are correct terms.
+            if (userPref.isVegan()) {
+                qp.addHealthLabels("vegan");
+            }
+            if (userPref.isVegetarian()) {
+                qp.addHealthLabels("vegetarian");
+            }
+            if (userPref.isDairy()) {
+                qp.addHealthLabels("dairy-free");
+            }
+            if (userPref.isEgg()) {
+                qp.addHealthLabels("egg-free");
+            }
+            if (userPref.isGluten()) {
+                qp.addHealthLabels("gluten-free");
+            }
+            if (userPref.isKosher()) {
+                qp.addHealthLabels("kosher");
+            }
+            if (userPref.isPaleo()) {
+                qp.addHealthLabels("paleo");
+            }
+            if (userPref.isPescatarian()) {
+                qp.addHealthLabels("pescatarian");
+            }
+            if (userPref.isShellfish()) {
+                qp.addHealthLabels("shellfish-free");
+            }
+            if (userPref.isPeanut()) {
+                qp.addHealthLabels("peanut-free");
+            }
+            if (userPref.isTreenut()) {
+                qp.addHealthLabels("tree-nut-free");
+            }
         }
-        if (userPref.isVegetarian()) {
-            qp.addHealthLabels("vegetarian");
-        }
-        if (userPref.isDairy()) {
-            qp.addHealthLabels("dairy-free");
-        }
-        if (userPref.isEgg()) {
-            qp.addHealthLabels("egg-free");
-        }
-        if (userPref.isGluten()) {
-            qp.addHealthLabels("gluten-free");
-        }
-        if (userPref.isKosher()) {
-            qp.addHealthLabels("kosher");
-        }
-        if (userPref.isPaleo()) {
-            qp.addHealthLabels("paleo");
-        }
-        if (userPref.isPescatarian()) {
-            qp.addHealthLabels("pescatarian");
-        }
-        if (userPref.isShellfish()) {
-            qp.addHealthLabels("shellfish-free");
-        }
-        if (userPref.isPeanut()) {
-            qp.addHealthLabels("peanut-free");
-        }
-        if (userPref.isTreenut()) {
-            qp.addHealthLabels("tree-nut-free");
-        }
-
     }
 }
